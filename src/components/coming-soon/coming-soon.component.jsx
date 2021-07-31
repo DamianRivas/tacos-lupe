0d9bdb5680d95179f69abfb11cdf4a3f00f201d3
damianrivas <drivas1993@gmail.com>
2021-07-31 10:39:22 -0700

    Fix layout shift from image loading

--- src/components/coming-soon/coming-soon.component.jsx
+++ src/components/coming-soon/coming-soon.component.jsx
@@ -1,12 +1,13 @@
 import { h } from "preact";
 import style from "./coming-soon.styles.scss";
 
-// import Logo from "/assets/images/tacos_lupe_transparent_trimmed.svg";
-
 const ComingSoon = () => (
   <section class={style["coming-soon"]}>
-    <img src="assets/images/tacos_lupe_transparent_trimmed.png" />
-    {/* <Logo /> */}
+    <img
+      src="assets/images/tacos_lupe_transparent_trimmed.png"
+      width="1200"
+      height="818"
+    />
     <h1>Coming Soon!</h1>
     <h2>
       <a href="tel:480-964-0025">(480) 964-0025</a>
