7d56b6268c5383e6cf61b985919c7216596726b2
damianrivas <drivas1993@gmail.com>
2021-08-09 19:21:37 -0700

    Implement linking to map apps on visit and navigate buttons

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -12,28 +12,50 @@ const backgroundImage = {
     url(/assets/images/food/carne-asada/carne-asada-xl.jpg)`,
 };
 
-const Hero = () => (
-  <div class={style.hero}>
-    <div class={style["background-image"]} style={backgroundImage} />
-    <div class={style.content}>
-      <h1 class={style.logo}>
-        <img
-          src="/assets/images/tacos_lupe_transparent_trimmed.png"
-          alt="Tacos Lupe Logo"
-        />
-      </h1>
-      <div class={style["button-row"]}>
-        <div>
-          <a href="tel:480-964-0025">
-            <CustomButton>Call</CustomButton>
-          </a>
-        </div>
-        <div>
-          <CustomButton>Visit</CustomButton>
+const Hero = () => {
+  const navLink = "https://g.page/tacos-lupe?share";
+
+  const mapsSelector = () => {
+    if (
+      /* if we're on iOS, open in Apple Maps */
+      navigator.platform.indexOf("iPhone") != -1 ||
+      navigator.platform.indexOf("iPad") != -1 ||
+      navigator.platform.indexOf("iPod") != -1
+    ) {
+      // template
+      // window.open("maps://maps.google.com/maps?daddr=<lat>,<long>&amp;ll=");
+      window.open(
+        // "maps://maps.google.com/maps?daddr=Tacos+Lupe+Mesa+AZ&amp;ll="
+        "maps://maps.google.com/maps?q=Tacos+Lupe+Mesa+AZ&sll=33.408364731900114,-111.80144021586207amp;ll="
+      );
+    } else {
+      window.open(navLink);
+    }
+  };
+
+  return (
+    <div class={style.hero}>
+      <div class={style["background-image"]} style={backgroundImage} />
+      <div class={style.content}>
+        <h1 class={style.logo}>
+          <img
+            src="/assets/images/tacos_lupe_transparent_trimmed.png"
+            alt="Tacos Lupe Logo"
+          />
+        </h1>
+        <div class={style["button-row"]}>
+          <div>
+            <a href="tel:480-964-0025">
+              <CustomButton>Call</CustomButton>
+            </a>
+          </div>
+          <div>
+            <CustomButton onclick={mapsSelector}>Visit</CustomButton>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Hero;
--- src/routes/visit/visit.component.jsx
+++ src/routes/visit/visit.component.jsx
@@ -4,37 +4,52 @@ import CustomButton from "../../components/custom-button/custom-button.component
 
 import style from "./visit.styles.scss";
 
-const Visit = () => (
-  <main class={style["visit-main"]}>
-    <section class={style["visit-container"]}>
-      <div class={style["visit-text"]}>
-        <h1>Visit Us</h1>
-        <p>
-          Stop by Tacos Lupe in Mesa, AZ today for the best tacos in Maricopa
-          County. You can also reach us by phone to learn more about our menu
-          and to ask any questions you may have. However you decide to reach out
-          to your local taqueria, we can't wait to hear from you!
-        </p>
-        <h3>Click here to navigate</h3>
-        <CustomButton
-          isLink={true}
-          href="https://www.google.com/maps/dir/?api=1&destination=Tacos+Lupe+Mesa+AZ"
-        >
-          Navigate
-        </CustomButton>
-      </div>
-      <div class={style["map-container"]}>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.574739595692!2d-111.8036610839906!3d33.40825725883635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba92f881fa9f1%3A0x7bed35b6f7951f82!2sTacos%20Lupe!5e0!3m2!1sen!2sus!4v1628545100279!5m2!1sen!2sus"
-          width="100%"
-          height="450"
-          style="border:0;aspect-ratio:16/9;"
-          allowfullscreen=""
-          loading="lazy"
-        />
-      </div>
-    </section>
-  </main>
-);
+const Visit = () => {
+  const navLink =
+    "https://www.google.com/maps/dir/?api=1&destination=Tacos+Lupe+Mesa+AZ";
+
+  const mapsSelector = () => {
+    if (
+      /* if we're on iOS, open in Apple Maps */
+      navigator.platform.indexOf("iPhone") != -1 ||
+      navigator.platform.indexOf("iPad") != -1 ||
+      navigator.platform.indexOf("iPod") != -1
+    ) {
+      window.open(
+        "maps://maps.google.com/maps/dir/?daddr=Tacos+Lupe+Mesa+AZ&amp;ll="
+      );
+    } else {
+      window.open(navLink);
+    }
+  };
+
+  return (
+    <main class={style["visit-main"]}>
+      <section class={style["visit-container"]}>
+        <div class={style["visit-text"]}>
+          <h1>Visit Us</h1>
+          <p>
+            Stop by Tacos Lupe in Mesa, AZ today for the best tacos in Maricopa
+            County. You can also reach us by phone to learn more about our menu
+            and to ask any questions you may have. However you decide to reach
+            out to your local taqueria, we can't wait to hear from you!
+          </p>
+          <h3>Click here to navigate</h3>
+          <CustomButton onclick={mapsSelector}>Navigate</CustomButton>
+        </div>
+        <div class={style["map-container"]}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.574739595692!2d-111.8036610839906!3d33.40825725883635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba92f881fa9f1%3A0x7bed35b6f7951f82!2sTacos%20Lupe!5e0!3m2!1sen!2sus!4v1628545100279!5m2!1sen!2sus"
+            width="100%"
+            height="450"
+            style="border:0;aspect-ratio:16/9;"
+            allowfullscreen=""
+            loading="lazy"
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Visit;
