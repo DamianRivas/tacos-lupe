281bdc10e4f81625ed325383027a3d357266294d
damianrivas <drivas1993@gmail.com>
2021-08-13 15:03:24 -0700

    Fix border around visit map

--- src/routes/visit/visit.component.jsx
+++ src/routes/visit/visit.component.jsx
@@ -38,14 +38,16 @@ const Visit = () => {
           <CustomButton onclick={mapsSelector}>Navigate</CustomButton>
         </div>
         <div class={style["map-container"]}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.574739595692!2d-111.8036610839906!3d33.40825725883635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba92f881fa9f1%3A0x7bed35b6f7951f82!2sTacos%20Lupe!5e0!3m2!1sen!2sus!4v1628545100279!5m2!1sen!2sus"
-            width="100%"
-            height="450"
-            style="border:0;aspect-ratio:16/9;"
-            allowfullscreen=""
-            loading="lazy"
-          />
+          <div class={style["map-border"]}>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.574739595692!2d-111.8036610839906!3d33.40825725883635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba92f881fa9f1%3A0x7bed35b6f7951f82!2sTacos%20Lupe!5e0!3m2!1sen!2sus!4v1628545100279!5m2!1sen!2sus"
+              width="100%"
+              height="450"
+              style="border:0;aspect-ratio:16/9;"
+              allowfullscreen=""
+              loading="lazy"
+            />
+          </div>
         </div>
       </section>
     </main>
