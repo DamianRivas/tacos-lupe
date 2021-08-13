381872d64c43fe573a43c37f2657fdcba58dab92
damianrivas <drivas1993@gmail.com>
2021-08-13 15:38:48 -0700

    Remove console.log for production

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,8 +3,6 @@ import { h } from "preact";
 import style from "./custom-button.styles.scss";
 
 const CustomButton = ({ children, isLink, xClass, ...otherProps }) => {
-  console.log("BUTTON PROPS:", otherProps);
-
   if (isLink) {
     return (
       <a class={`${style["custom-button"]} ${xClass}`} {...otherProps}>
