ee4e8a6eb8fe2042bfb2661445559ccda5d50e2b
damianrivas <drivas1993@gmail.com>
2021-08-25 18:46:45 -0700

    Clean up comments

--- src/components/content-section/content-section.component.jsx
+++ src/components/content-section/content-section.component.jsx
@@ -2,32 +2,25 @@ import { h } from "preact";
 
 import style from "./content-section.styles.scss";
 
-const ContentSection = ({ title, body, imageSrc = null, key }) => {
-  // const backgroundImage = {
-  //   "background-image": `url(${imageSrc})`,
-  // };
-
-  return (
-    <section key={key} class={style["c-split"]}>
-      {imageSrc ? (
-        <div class={style["img-container"]}>
-          {/* <div class={style["content-img"]} style={backgroundImage} /> */}
-          <picture>
-            <img
-              src={imageSrc}
-              alt="Image of food served at Tacos Lupe"
-              width="160"
-              height="90"
-            />
-          </picture>
-        </div>
-      ) : null}
-      <div class={style["content-body"]}>
-        <h2>{title}</h2>
-        <p>{body}</p>
+const ContentSection = ({ title, body, imageSrc = null, key }) => (
+  <section key={key} class={style["c-split"]}>
+    {imageSrc ? (
+      <div class={style["img-container"]}>
+        <picture>
+          <img
+            src={imageSrc}
+            alt="Image of food served at Tacos Lupe"
+            width="160"
+            height="90"
+          />
+        </picture>
       </div>
-    </section>
-  );
-};
+    ) : null}
+    <div class={style["content-body"]}>
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </div>
+  </section>
+);
 
 export default ContentSection;
